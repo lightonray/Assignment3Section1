af8e5f699eabe8ed6ff1b137b7c1aadecae8a355
American Computers <American Computers@LIGHTONRAY>
2021-12-10 01:12:40 +0200

    Add project files.

---
+++ Assignment3Section1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace Assignment3MaxMinSumAverageOfArray
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            try
+            {
+                double[] numbers = new double[4];
+
+                Console.WriteLine("Please insert four numbers");
+
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    numbers[i] = Convert.ToDouble(Console.ReadLine());
+                }
+
+                double max = numbers.Max();
+                double min = numbers.Min();
+                double sum = numbers.Sum();
+                double average = (double)numbers.Average();
+
+                Console.WriteLine("Max value is: " + max + "\nMin value is: " + min + "\nThe sum is: " + sum + "\nThe average is: " + average);
+            }
+            catch
+            {
+                Console.Write("Please enter a valid value");
+            }
+        }
+    }
+}
